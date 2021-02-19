ced5ad429a968caa0df389ddf52acc578f772176
Minna Isokorpi <AA4319@student.jamk.fi>
2021-02-19 16:37:05 +0200

    Reititys kuntoon ja yks typo korjattu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { MeComponent } from './me/me.component';
+import { StudiesComponent } from './studies/studies.component';
 
-const routes: Routes = [];
+// Reitit tulevat routes-taulukkoon, taulukon olioiden tietotyyppinä Routes
+const routes: Routes = [
+  { path: 'me', component: MeComponent },
+  { path: 'studies', component: StudiesComponent },
+  { path: 'hobbies', component: HobbiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
